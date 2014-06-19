af5cccd722689fa05f11e91753bddf268402d21c
Mitar <mitar.git@tnode.com>
2014-06-18 23:10:31 -0700

    Factored command-line utility out.

--- tests/convertXml.js
+++ tests/convertXml.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-var async = require('async');
-var fs = require('fs');
-var util = require('util');
-var xml4js = require('../xml4js');
-var _ = require('underscore');
+var xml4json = require('../lib/xml4json');
 
 var SCHEMAS = {
   'http://www.example.com/PO': './xml/po.xsd',
@@ -12,47 +8,6 @@ var SCHEMAS = {
   'http://www.example.com/Report': './xml/report.xsd'
 };
 
-var parser = new xml4js.Parser({
+xml4json({
   downloadSchemas: false
-});
-
-async.each(_.keys(SCHEMAS), function (namespace, cb) {
-  var files = SCHEMAS[namespace];
-  if (!_.isArray(files)) {
-    files = [files];
-  }
-  async.each(files, function (file, cb) {
-    var content = fs.readFileSync(file, {encoding: 'utf-8'});
-    parser.addSchema(namespace, content, cb);
-  }, cb)
-}, function (err) {
-  if (err) {
-    console.error('' + err);
-    process.exit(2);
-    return;
-  }
-
-  process.stdin.resume();
-  process.stdin.setEncoding('utf-8');
-
-  var input = '';
-  process.stdin.on('data', function (chunk) {
-    input += chunk;
-  }).on('end', function () {
-    parser.parseString(input, function (err, result) {
-      if (err) {
-        if (err.stack) {
-          console.error(err.stack);
-        }
-        else {
-          console.error('' + err);
-        }
-        process.exit(1);
-      }
-      else {
-        console.log(JSON.stringify(result, null, 2));
-        process.exit(0);
-      }
-    });
-  });
-});
+}, SCHEMAS);
--- xml4json.js
+++ xml4json.js
@@ -1,30 +1,7 @@
 #!/usr/bin/env node
 
-var util = require('util');
-var xml4js = require('./xml4js');
+var xml4json = require('./lib/xml4json');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-var input = '';
-process.stdin.on('data', function (chunk) {
-  input += chunk;
-}).on('end', function () {
-  xml4js.parseString(input, {
-    downloadSchemas: true
-  }, function (err, result) {
-    if (err) {
-      if (err.stack) {
-        console.error(err.stack);
-      }
-      else {
-        console.error('' + err);
-      }
-      process.exit(1);
-    }
-    else {
-      console.log(JSON.stringify(result, null, 2));
-      process.exit(0);
-    }
-  });
+xml4json({
+  downloadSchemas: true
 });
